fb66c4202b213e884bdac8d519561a547fa50e10
yaminiudata135791 <yaminiudata@gmail.com>
2022-04-12 19:03:10 +0530

    DlitheProjectTask1

---
+++ Back-end/Task1/PrivateTask.java
@@ -0,0 +1,11 @@
+package com.Task1;
+
+public class PrivateTask {
+     public static void main(String[] args){
+        PublicTask publicTask=new PublicTask();
+        publicTask.addition();
+        publicTask.substraction();
+        publicTask.multiplication();
+
+     }
+}
---
+++ Back-end/Task1/PublicTask.java
@@ -0,0 +1,20 @@
+package com.Task1;
+
+public class PublicTask {
+        int a=30,b=20,result;
+
+    public void addition(){
+            result=a+b;
+            System.out.println("sum of two numbers is"+result);
+        }
+        public void substraction(){
+            result=a-b;
+            System.out.println("Sub of two numbers is"+result);
+        }
+        public void multiplication(){
+            result=a*b;
+            System.out.println("mul of two numbers is"+result);
+        }
+    }
+
+
